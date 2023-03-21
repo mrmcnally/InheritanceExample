13e41ddcdf5129e878f8d4f8f926a974324ecc91
mrmcnally <cmcnally924@c2ken.net>
2023-03-21 17:01:29 +0000

    Add Vehicle Interface and Car object to implement it. Remove IAnimal

---
+++ InheritanceExample/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    class Car : IVehicle
+    {
+        private string name;
+        private int numberOfPassengers;
+        private string engineType;
+        private int topSpeed;
+
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+        public int NumberOfPassengers 
+        {
+            get { return numberOfPassengers; }
+            set { numberOfPassengers = value; }
+        }
+        public string EngineType 
+        {
+            get { return engineType; }
+            set { engineType = value; }
+        }
+        public int TopSpeed 
+        {
+            get { return topSpeed; }
+            set { topSpeed = value; }
+        }
+
+        public void getDetails()
+        {
+            Console.WriteLine("Name: {0}   No of Passengers: {1} ... etc", Name, NumberOfPassengers);
+        }
+
+        public string passengersToString()
+        {
+            return NumberOfPassengers.ToString();
+        }
+    }
+}
---
+++ InheritanceExample/IVehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    interface IVehicle
+    {
+        //interface properties
+        string Name { get; set; }
+        int NumberOfPassengers { get; set; }
+        string EngineType { get; set; }
+        int TopSpeed { get; set; }
+         
+        //interface methods
+        void getDetails();
+        string passengersToString(); 
+
+    }
+}
+
