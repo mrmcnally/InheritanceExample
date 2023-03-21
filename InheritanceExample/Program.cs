51a2e899cc7c8db307becfdf5637d1c092844613
mrmcnally <cmcnally924@c2ken.net>
2023-03-21 15:31:49 +0000

    IAnimal interface added

--- InheritanceExample/Program.cs
+++ InheritanceExample/Program.cs
@@ -11,19 +11,19 @@ namespace InheritanceExample
             Dog d1 = new Dog("Derived Dog", "Pug");
 
             //instantiating an object using the base class reference
-            Animal a1 = new Animal("Random Animal");
+            //Animal a1 = new Animal("Random Animal");
             Animal a2 = new Dog("Dog", "Poodle");
             Animal a3 = new Cat("Cat", "Balinese");
 
             //making sounds
             d1.makeSound();
-            a1.makeSound();
+            //a1.makeSound();
             a2.makeSound();
             a3.makeSound();
 
             Console.WriteLine("Iterating through Animal Array");
             Animal[] AnimalArray = new Animal[3];
-            AnimalArray[0] = new Animal("Random Animal");
+            //AnimalArray[0] = new Animal("Random Animal");
             AnimalArray[1] = new Dog("Dog", "Pug");
             AnimalArray[2] = new Cat("Cat", "Balinese");
 
